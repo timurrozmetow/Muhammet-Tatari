3c84805ba673062dc842aa66edf20ce4ddb4f46e
Rahmanbayramov <bayramovrahman9@gmail.com>
2025-01-03 11:41:56 +0500

    update tm

--- src/i18n.js
+++ src/i18n.js
@@ -604,8 +604,8 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en",
-  fallbackLng: "en",
+  lng: "tm",
+  fallbackLng: "tm",
   interpolation: {
     escapeValue: false,
   },
